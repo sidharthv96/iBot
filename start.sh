65282b4fee7bc9f38136d46896ed29b6da5a2a56
Sidharth V <sidharthv96@gmail.com>
2018-04-03 09:48:28 +0530

    Modify Startup Script - Log

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ if [ $? == 0 ]; then
 fi
 ps -a | grep 'python' &> /dev/null
 if [ $? != 0 ]; then
-    sudo /home/pi/.local/share/virtualenvs/iBot-yjzPlzR6/bin/python manage.py runserver 0.0.0.0:80 --settings=iBot.production_settings &>> logs
+    sudo /home/pi/.local/share/virtualenvs/iBot-yjzPlzR6/bin/python manage.py runserver 0.0.0.0:80 --settings=iBot.production_settings > log 2>&1 &
 fi
 
