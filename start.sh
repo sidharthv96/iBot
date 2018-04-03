8e348a8a8e7aa48bcfc0e700edf2e983d24b29f8
Sidharth V <sidharthv96@gmail.com>
2018-04-03 09:30:40 +0530

    Modify Startup Script

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-git pull
-cd frontend
-npm start build &
-cd ..
+git pull | grep 'up-to-date' &> /dev/null
+if [ $? == 0 ]; then
+   cd frontend
+    npm run build &
+    cd ..
+fi
 sudo /home/pi/.local/share/virtualenvs/iBot-yjzPlzR6/bin/python manage.py runserver 0.0.0.0:80 --settings=iBot.production_settings &
