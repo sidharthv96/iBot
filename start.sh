94ca5fdd89e1543c3af5e83c5daeb9216b5c63d3
Sidharth V <sidharthv96@gmail.com>
2018-04-03 10:02:02 +0530

    Modify Startup Script - Log

--- start.sh
+++ start.sh
@@ -9,5 +9,6 @@ fi
 ps -a | grep 'python' &> /dev/null
 if [ $? != 0 ]; then
     sudo /home/pi/.local/share/virtualenvs/iBot-yjzPlzR6/bin/python manage.py runserver 0.0.0.0:80 --settings=iBot.production_settings > log 2>&1 &
+    python ~/ardublockly/start.py -b
 fi
 
