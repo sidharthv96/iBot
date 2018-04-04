ed3e143f4df229080e9af36ba6b64c0daa9d0bc1
Sidharth V <sidharthv96@gmail.com>
2018-04-04 12:36:44 +0530

    DB

--- iBot/settings.py
+++ iBot/settings.py
@@ -84,9 +84,17 @@ WEBPACK_LOADER = {
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'ibot',
+        'USER': 'ibot',
+        'PASSWORD': 'ibotpass',
+        'HOST': 'localhost',
     }
 }
 
