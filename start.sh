06d33cd0c4847196f5711284c1f02630f0835f61
Sidharth V <sidharthv96@gmail.com>
2018-04-04 11:52:23 +0530

    Start

--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@ git reset --hard origin/master
 git pull | grep 'frontend' &> /dev/null
 if [ $? == 0 ]; then
    cd frontend
-#   npm run build
-   npm run start &
+   npm run build
+#   npm run start &
    cd ..
 fi
 ps -a | grep 'python' &> /dev/null
 if [ $? != 0 ]; then
     echo "Python Started"
-    sudo /home/pi/.local/share/virtualenvs/iBot-yjzPlzR6/bin/python manage.py runserver 0.0.0.0:80 > iBot_log 2>&1 &
-#    sudo /home/pi/.local/share/virtualenvs/iBot-yjzPlzR6/bin/python manage.py runserver 0.0.0.0:80 --settings=iBot.production_settings > iBot_log 2>&1 &
+#    sudo /home/pi/.local/share/virtualenvs/iBot-yjzPlzR6/bin/python manage.py runserver 0.0.0.0:80 > iBot_log 2>&1 &
+    sudo /home/pi/.local/share/virtualenvs/iBot-yjzPlzR6/bin/python manage.py runserver 0.0.0.0:80 --settings=iBot.production_settings > iBot_log 2>&1 &
     python ~/ardublockly/start.py -b > blockly_log 2>&1 &
 fi
 
